415d73fa04c9f891f4d059af4b9c32e9a9e40078
ppsoldier <35059159@qq.com>
2026-06-25 19:48:47 +0800

    Update __init__.py

--- stock-watch-service/data_collector/__init__.py
+++ stock-watch-service/data_collector/__init__.py
@@ -1,1 +1,7 @@
+"""数据采集模块"""
 
+from .eastmoney import EastMoneyCollector
+from .tonghuashun import TongHuaShunCollector
+from .jiufang import JiuFangCollector
+
+__all__ = ["EastMoneyCollector", "TongHuaShunCollector", "JiuFangCollector"]
