4e6f6301f570bd449459fabd2a6551f69f6e7b68
ppsoldier <35059159@qq.com>
2026-07-01 22:49:18 +0800

    Update jiufang.py

--- stock-watch-service/data_collector/jiufang.py
+++ stock-watch-service/data_collector/jiufang.py
@@ -17,7 +17,7 @@ import requests
 import pandas as pd
 from loguru import logger
 
-from config import DATA_SOURCES, STOCK_FILTER, DATA_CACHE_DIR, PERFORMANCE, LLM_CONFIG, REQUEST_TIMEOUT, PERFORMANCE, MAX_STOCK_PAGES
+from config import DATA_SOURCES, STOCK_FILTER, DATA_CACHE_DIR, PERFORMANCE, REQUEST_TIMEOUT, PERFORMANCE, MAX_STOCK_PAGES, LLM_CONFIG_SILICON, LLM_CONFIG_DASHSCOPE  # 新增这两个
 
 # API 基础地址
 API_BASE = "https://api-hq.chongnengjihua.com/finance/api/2/stock/a/rank/list"
